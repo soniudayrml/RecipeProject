0896ea441de3d0ecc0f6480785376e26da3283ec
soniudayrml <soniudayrml@gmail.com>
2021-09-14 12:33:56 +0530

    commit

---
+++ src/main/java/com/recipe/project/domain/UnitOfMeasurement.java
@@ -0,0 +1,27 @@
+package com.recipe.project.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
